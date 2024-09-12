988a30c1494b8d98f2f1c6fd1de000618a26545a
Virecny Stephanie Mata Toyo <162191678+Stephaniemtoyo@users.noreply.github.com>
2024-09-12 18:49:54 +0000

    cambios2

--- src/models.py
+++ src/models.py
@@ -25,9 +25,9 @@ class User(db.Model):
 class Characters(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
-    gender = db.Column(db.String(50), unique=True, nullable=False)
-    hair_color = db.Column(db.String(50), unique=True, nullable=False)
-    eye_color = db.Column(db.String(50), unique=True, nullable=False)
+    gender = db.Column(db.String(50), unique=False, nullable=False)
+    hair_color = db.Column(db.String(50), unique=False, nullable=False)
+    eye_color = db.Column(db.String(50), unique=False, nullable=False)
     
     def _repr_(self):
         return '<Characters %r>' % self.id
@@ -38,4 +38,59 @@ class Characters(db.Model):
             "gender": self.gender,
             "hair_color": self.hair_color,
             "eye_color": self.eye_color
+        }
+    
+class Favourites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref = db.backref('users', lazy=True))
+    characters_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+    characters = db.relationship('Characters', backref=db.backref('characters', lazy=True))
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    planets = db.relationship('Planets', backref=db.backref('planets', lazy=True))
+    vehicles_id = db.Column(db.Integer, db.ForeignKey('vehicles.id'))
+    vehicles = db.relationship('Vehicles', backref=db.backref('vehicles', lazy=True))
+    def _repr_(self):
+        return '<Favourites %r>' % self.id
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "characters_id": self.characters_id,
+            "planets_id": self.planets_id,
+            "vehicles_id": self.vehicles_id
+        }
+    
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    Terrain = db.Column(db.String(50), unique=True, nullable=False)
+    population = db.Column(db.Integer,unique=True, nullable=False)
+    def _repr_(self):
+        return '<Planets %r>' % self.id
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "Terrain": self.Terrain,
+            "population": self.population
+        }
+    
+
+class Vehicles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    model = db.Column(db.String(50), unique=True, nullable=False)
+    Cost_in_Credits = db.Column(db.Integer,unique=True, nullable=False)
+    Crew = db.Column(db.Integer,unique=True, nullable=False)
+    def _repr_(self):
+        return '<Vehicles %r>' % self.name
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "Cost_in_Credits": self.Cost_in_Credits,
+            "Crew": self.Crew
         }
